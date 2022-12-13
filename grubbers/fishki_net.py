3a35067610c91b98f87d7443726b2059e745abc0
Your Name <you@example.com>
2022-12-13 14:43:53 +0200

    добавил парсер сайта fishki.net

--- grubbers/fishki_net.py
+++ grubbers/fishki_net.py
@@ -6,7 +6,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def zaebovnet_pars(uri_part: str):
+def fishkinet_pars(uri_part: str):
     mem_info_dict = dict()
     memes_list = list()
     ua = UserAgent()
@@ -15,63 +15,44 @@ def zaebovnet_pars(uri_part: str):
         "user-agent": ua.chrome
     }
 
-    domen = "https://zaebov.net"
+    domen = "https://fishki.net"
     print(headers["user-agent"])
-    for x in range(1, 27):
-        uri_string = f"/{uri_part}/page/{x}" if x > 1 else f"/{uri_part}"
+    for x in range(1, 206):
+        uri_string = f"/{uri_part}/{x}"
+
         url_string = domen + uri_string
-        # print(uri + " " + str(datetime.datetime.now()))
+        print(url_string)
         try:
             res = requests.get(url_string, headers=headers)
-            # print(res.text)
             if res.status_code == 200:
                 print(res.status_code)
                 logging.info(f'Рабатаю со страницей - {x}')
                 print(f'Рабатаю со страницей - {x}')
                 soup = BeautifulSoup(res.text, "lxml")
-                divs = soup.find_all("div", class_="entry-title")
-                # print(divs)
-                for div in divs:
-                    img_page_url = div.find("a").get("href")
-                    img_page_name = div.find("a").text
+                img_divs = soup.find_all("div", class_="slide__item")
+
+                for img_div in img_divs:
                     try:
-                        res = requests.get(img_page_url, headers=headers)
-                        # print(res.text)
-                        if res.status_code == 200:
-                            print(res.status_code)
-                            logging.info(f'Рабатаю с набором мемов - {img_page_name}')
-                            print(f'Рабатаю с набором мемов - {img_page_name}')
-                            soup = BeautifulSoup(res.text, "lxml")
-                            div_entry_content = soup.find("div", class_="entry-content")
-                            pp = div_entry_content.find_all("p")
-                            # print(pp)
-
-                            for page in pp:
-                                try:
-                                    img_url = page.find("img").get("src")
-                                    print(img_url)
-                                    if img_url:
-                                        mem_info_dict["name"] = None
-                                        mem_info_dict["img"] = img_url
-                                        buffer = mem_info_dict.copy()
-                                        memes_list.append(buffer)
-                                        mem_info_dict.clear()
-                                except Exception as eeee:
-                                    print(f"Проблеммы с отдельным изображением - {eeee}")
-                                    logging.error(f"Проблеммы с отдельным изображением - {eeee}")
-                                finally:
-                                    continue
-
-                    except Exception as eee:
-                        print(f"Проблеммы с requests на странице с мемами - {eee}")
-                        logging.error(f"Проблеммы с requests на странице с мемами - {eee}")
+                        img_url = img_div.find("img").get("src")
+                        print(img_url)
+                        if img_url:
+                            mem_info_dict["name"] = None
+                            mem_info_dict["img"] = img_url
+                            buffer = mem_info_dict.copy()
+                            memes_list.append(buffer)
+                            mem_info_dict.clear()
+                    except Exception as eeee:
+                        print(f"Проблеммы с отдельным изображением - {eeee}")
+                        logging.error(f"Проблеммы с отдельным изображением - {eeee}")
+                    finally:
+                        continue
 
         except Exception as e:
             print(e)
-            logging.error(f"Проблеммы с requests на странице с наборами - {e}")
+            logging.error(f"Проблеммы с requests на странице с мемами - {e}")
 
     try:
-        with open("../json_files/zaebovnet_memes.json", "w", encoding="utf-8") as f:
+        with open("../json_files/fishkinet_memes.json", "w", encoding="utf-8") as f:
             json.dump(memes_list, f, indent=4, ensure_ascii=False)
         logging.info(f"В итоговом файле {len(memes_list)} записей")
     except Exception as ee:
@@ -82,7 +63,7 @@ if __name__ == "__main__":
     # logging.basicConfig(level=logging.INFO, filename="../logs/memesmix_log.log", filemode="w",
     # format="%(asctime)s %(levelname)s %(message)s")
 
-    logging.basicConfig(level=logging.INFO, filename="../logs/zaebovnet_log.log", filemode="w",
+    logging.basicConfig(level=logging.INFO, filename="../logs/fishkinet_log.log", filemode="w",
                         format="%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s")
 
-    zaebovnet_pars("foto-prikoli")
+    fishkinet_pars("kartinka-dnja")
