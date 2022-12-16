4ac1b52706ef8f47487286dc18fab0ad640be98d
Your Name <you@example.com>
2022-12-16 22:23:07 +0200

    добавил парсер сайта https://anekdot.ru

--- mems_memesmix_grubber.py
+++ mems_memesmix_grubber.py
@@ -7,10 +7,11 @@ from database.db import db_connect
 def make_list() -> list:
     # memesmix_list = list()
     bugaga_list = list()
-    pinterest_list = list()
+    # pinterest_list = list()
     fishki_list = list()
     demotos_list = list()
     zaebovnet_list = list()
+    anekdot_list = list()
 
     total_list = list()
     pint_dict = {
@@ -18,13 +19,13 @@ def make_list() -> list:
         "img": ""
     }
 
-    with open("json_files/result.txt", "r", encoding="utf-8") as file:
+    with open("json_files/anekdot_memes.txt", "r", encoding="utf-8") as file:
         pinterest = file.readlines()
     for x in pinterest:
-        pint_dict["img"] = x.strip()
-        pint_dict["name"] = ""
-        pinterest_list.append(pint_dict.copy())
-        pint_dict.clear()
+        # pint_dict["img"] = x.strip()
+        # pint_dict["name"] = ""
+        anekdot_list.append(x)
+        # pint_dict.clear()
 
     # with open("json_files/memesmix_memes.json", "r", encoding="utf-8") as file:
     #     memesmix_list = json.load(file)
@@ -40,16 +41,20 @@ def make_list() -> list:
     with open("json_files/zaebovnet_memes.json", "r", encoding="utf-8") as file:
         zaebovnet_list = json.load(file)
 
-    total_list = [*bugaga_list, *pinterest_list, *demotos_list, *fishki_list, *zaebovnet_list]
+    total_list = [*bugaga_list, *demotos_list, *fishki_list, *zaebovnet_list]
     shuffle(total_list)
-    print(len(pinterest_list))
+    # print(len(pinterest_list))
     # print(len(memesmix_list))
-    print(len(bugaga_list))
-    print(len(fishki_list))
-    print(len(demotos_list))
-    print(len(zaebovnet_list))
+    # print(len(bugaga_list))
+    # print(len(fishki_list))
+    # print(len(demotos_list))
+    # print(len(zaebovnet_list))
 
-    print(len(total_list))
+    an = set()
+    an.update(anekdot_list)
+    print(len(anekdot_list))
+    print(len(an))
+    # print(len(total_list))
     #
     # with open("json_files/result_memes.json", "w", encoding="utf-8") as file:
     #     json.dump(total_list, file, ensure_ascii=False, indent=4)
@@ -61,4 +66,5 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, filename="logs/memes_log.log", filemode="w",
                         format="%(asctime)s %(levelname)s %(message)s")
 
-    db_connect(make_list())
+    make_list()
+    # db_connect(make_list())
